4b6aceb1ef06f6f134fcfccc84b121be643137af
Jussi Räsänen <jussi@proton.me>
2022-06-12 16:11:24 +0300

    add more palettes, chunk palettes into 30 colors

--- source/index.js
+++ source/index.js
@@ -1,10 +1,27 @@
 import { promises as fs } from "fs";
 import { readSwatchesFile, createSwatchesFile } from "procreate-swatches";
 import axios from "axios";
+import _ from "lodash";
 
-const ALL_PALETTES = ["resurrect-64", "endesga-32"];
+const ALL_PALETTES = [
+  "resurrect-64",
+  "endesga-32",
+  "aap-64",
+  "pear36",
+  "apollo",
+  "lospec500",
+  "endesga-64",
+  "journey",
+  "vinik24",
+  "sweetie-16",
+  "slso8",
+  "fantasy-24",
+  "zughy-32",
+  "cc-29",
+  "famicube",
+];
 
-const LOSPEC_BASE_URL = `https://lospec.com/palette-list/`;
+const LOSPEC_BASE_URL = `https://lospec.com/palette-list`;
 
 const getSwatchFile = async (name) => {
   const { data } = await axios.get(`${LOSPEC_BASE_URL}/${name}.json`);
@@ -18,21 +35,34 @@ const swatchToRgb = (colors) => {
   return rgbColors;
 };
 
+const getTitle = (name, index, author) =>
+  author ? `${name} - ${author} - ${index}` : `${name} - ${index}`;
+
 (async () => {
   await Promise.all(
     ALL_PALETTES.map(async (current) => {
       const swatch = await getSwatchFile(current);
       const name = swatch.name;
-      const colors = swatchToRgb(swatch.colors);
+      const author = swatch?.author || null;
+      // One procreate palette supports only 30 colors
+      const chunkedColors = _.chunk(swatchToRgb(swatch.colors), 30);
 
-      console.log(colors);
+      const totalColors = swatch.colors.length;
+      const totalChunks = chunkedColors.length;
+      console.log(`Palette has ${totalColors}, ${totalChunks} files`);
 
-      const swatchesFile = await createSwatchesFile(name, colors);
-      console.log(swatchesFile);
+      return chunkedColors.map(async (colors, index) => {
+        const current = index + 1;
 
-      fs.writeFile(`output/${name}.swatches`, Buffer.from(swatchesFile));
+        const title = getTitle(name, current, author);
 
-      return Promise.resolve();
+        const swatchesFile = await createSwatchesFile(title, colors);
+        console.log(`Saving ${name} (${current}/${totalChunks})`);
+        return fs.writeFile(
+          `output/${title}.swatches`,
+          Buffer.from(swatchesFile)
+        );
+      });
     })
-  );
+  ).catch(console.error);
 })();
